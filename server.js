df5c6b739ccc3d7634fc3cf8d05cd1606b6e1a5e
damarowen <damarowen@gmail.com>
2020-11-12 21:38:25 +0700

    add router

--- server.js
+++ server.js
@@ -5,6 +5,32 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+//route structur
+
+
+app.get('/api/v1/bootcamp', (req,res) => {
+    res.status(200).json({ success: true, msg: "show all bootcamps"})
+})
+
+app.post('/api/v1/bootcamp', (req,res) => {
+    res.status(200).json({ success: true, msg: "created bootcamp"})
+})
+
+app.get('/api/v1/bootcamp/:id', (req,res) => {
+    res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}`})
+})
+
+app.put('/api/v1/bootcamp/:id', (req,res) => {
+    res.status(200).json({ success: true, msg: `edit bootcamp ${req.params.id}`})
+})
+
+app.delete('/api/v1/bootcamp/:id', (req,res) => {
+    res.status(200).json({ success: true, msg: `delete bootcamp ${req.params.id}`})
+})
+
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
