994f1fba8aeb7cbc1d160d212e56adbe04315053
damarowen <damarowen@gmail.com>
2021-01-19 17:41:32 +0700

    fix bug

--- controllers/users.js
+++ controllers/users.js
@@ -2,18 +2,17 @@ const asyncHandler = require('../middleware/async')
 const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 
-
 // @desc  GET ALL USER
-// @route GET /api/v1/auth/users
+// @route GET /api/v1/users
 // @access  Private/admin
 
 exports.getUsers = asyncHandler(async (req, res, next) => {
 
-    res.status(200).json(res.advancedResults);
+    res.status(200).json(res.advanceResults);
 })
 
 // @desc  GET SINGLE USER
-// @route GET /api/v1/auth/userS/:id
+// @route GET /api/v1/userS/:id
 // @access  Private/admin
 
 exports.getUser = asyncHandler(async (req, res, next) => {
@@ -28,7 +27,7 @@ exports.getUser = asyncHandler(async (req, res, next) => {
 
 
 // @desc  CREATE user
-// @route POST /api/v1/auth/users
+// @route POST /api/v1/users
 // @access  Private/admin
 
 exports.createUser = asyncHandler(async (req, res, next) => {
@@ -43,7 +42,7 @@ exports.createUser = asyncHandler(async (req, res, next) => {
 
 
 // @desc  UPDATE user
-// @route PUT /api/v1/auth/users/:id
+// @route PUT /api/v1/users/:id
 // @access  Private/admin
 
 exports.updateUser = asyncHandler(async (req, res, next) => {
@@ -60,7 +59,7 @@ exports.updateUser = asyncHandler(async (req, res, next) => {
 
 
 // @desc  DELETE user
-// @route DELETE /api/v1/auth/users/:id
+// @route DELETE /api/v1/users/:id
 // @access  Private/admin
 
 exports.deleteUser = asyncHandler(async (req, res, next) => {
